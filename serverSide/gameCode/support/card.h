d16d8699d794ebf393175ce6c0a50511966edcb6
PIlotA330 <69285986+PIlotA330@users.noreply.github.com>
2025-02-25 15:44:24 -0600

    started fleshing out the classes

--- serverSide/gameCode/support/card.h
+++ serverSide/gameCode/support/card.h
@@ -5,4 +5,15 @@
 #ifndef CARD_H
 #define CARD_H
 
+class card {
+public:
+	card();
+	int getNumber();
+	int getSuit();
+
+private:
+	int number;
+	int suit;
+};
+
 #endif //CARD_H
--- serverSide/gameCode/support/deck.h
+++ serverSide/gameCode/support/deck.h
@@ -4,11 +4,16 @@
 
 #ifndef DECK_H
 #define DECK_H
+#include <vector>
+#include "card.h"
 
 class deck {
 public:
     deck();
   	deck *getDeckPointer();
+
+private:
+	std::vector<std::shared_ptr<card>> cards;
 
 };
 #endif //DECK_H
